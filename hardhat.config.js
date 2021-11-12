46bcb5ba62780d271da7885e14fce1e195e5f89e
Ilya <fadeev.ilya@gmail.com>
2021-11-12 00:51:00 -0500

    configured hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,37 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-waffle")
+const fs = require('fs')
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const projectId = fs.readFileSync('./.project-id.txt', {
+  encoding: 'utf8',
+  flag: 'r'
+})
+const keyData = fs.readFileSync('./.p-key.txt', {
+  encoding: 'utf8',
+  flag: 'r'
+})
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 1337 // standard for local
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [keyData]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [keyData]
+    }
+  }
+}
