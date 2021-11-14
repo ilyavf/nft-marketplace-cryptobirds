680786033413e6d7d615e39711a6aa22c5633703
Ilya <fadeev.ilya@gmail.com>
2021-11-14 02:33:40 -0500

    enabled tailwind; added KBMarket nav

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
 import '../styles/globals.css'
+import './app.css'
+import Link from 'next/link'
+
+function KBMarketplace({ Component, pageProps }) {
+  return (
+    <div>
+      <nav className='border-b p-6' style={{backgroundColor: 'purple'}}>
+        <p className='text-4xl font-bold text-white text-center'>KryptoBird Marketplace</p>
+        <div className='flex mt-4 justify-center'>
+          <Link href='/'>
+            <a className='mr-6'>Main Marketplace</a>
+          </Link>
+          <Link href='/mint-item'>
+            <a className='mr-6'>Mint Tokens</a>
+          </Link>
+          <Link href='/my-nft'>
+            <a className='mr-6'>My NFTs</a>
+          </Link>
+          <Link href='/dashboard'>
+            <a className='mr-6'>Account Dashboard</a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
+}
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default KBMarketplace
