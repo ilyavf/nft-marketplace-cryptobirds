8372d214743ef91964bc10de05b52bbd168fe64d
Ilya <fadeev.ilya@gmail.com>
2021-11-14 02:54:28 -0500

    created deploy script; save contract addresses to the config file

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ npx hardhat node
 node scripts/sample-script.js
 npx hardhat help
 ```
+
+Deploy with a local node:
+```
+$ npx hardhat node
+Started HTTP and WebSocket JSON-RPC server at http://127.0.0.1:8545/
+...
+
+$ npx hardhat run scripts/deploy.js --network localhost
+KBMarket contract deployed to: 0x5FbDB2315678afecb367f032d93F642f64180aa3
+NFT contract deployed to: 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+```
---
+++ scripts/deploy.js
@@ -0,0 +1,34 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require('hardhat');
+const fs = require('fs')
+
+async function main() {
+  // We get the contract to deploy
+  const KBMarket = await hre.ethers.getContractFactory('KBMarket');
+  const kbMarket = await KBMarket.deploy();
+  await kbMarket.deployed();
+  console.log('KBMarket contract deployed to:', kbMarket.address);
+
+  const NFT = await hre.ethers.getContractFactory('NFT');
+  const nft = await NFT.deploy(kbMarket.address);
+  await nft.deployed();
+  console.log('NFT contract deployed to:', nft.address);
+
+  let config = `
+  export const nftmarketaddress = ${kbMarket.address}
+  export const nftaddress = ${nft.address}`
+
+  let data = JSON.stringify(config)
+  fs.writeFileSync('config.js', JSON.parse(data))
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
